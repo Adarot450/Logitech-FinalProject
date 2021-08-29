b2ecca0634314c8e679e78fa7fcf8c7833dc9428
Adam <adamramy337@gmail.com>
2021-08-29 09:38:20 +0200

    All products+more organization

---
+++ js/allProducts.js
@@ -0,0 +1,98 @@
+const allProducts = [
+  {
+    name: "G513",
+    description:
+      "The G513 is a mechanical RGB keyboared with 12 programable keys",
+    Rating: "Rating:9.1/10",
+    Price: "Price:130$-150$",
+    img: "../ress/g513.webp",
+  },
+  {
+    name: "G613",
+    description:
+      "The G513 is a mechanical RGB keyboared with 12 programable keys",
+    Rating: "Rating:7.5/10",
+    Price: "Price:150$",
+    img: "../ress/g613.webp",
+  },
+  {
+    name: "G903",
+    description:
+      "The G903 is a RGB wireless mouse with 6 programable buttons and with upto 140 hours of lifetime from one charge.",
+    Rating: "Rating:8/10",
+    Price: "Price:130$",
+    img: "../ress/g903.webp",
+  },
+  {
+    name: "G502",
+    description:
+      "The G502 is a RGB wireless mouse with many programable buttons.",
+    Rating: "Rating:7.8/10",
+    Price: "Price:150$",
+    img: "../ress/g502.webp",
+  },
+  {
+    name: "G903",
+    description: "The G903 is a Gaming headset with Spacial ingame awarness.",
+    Rating: "Rating:8/10",
+    Price: "Price:130$",
+    img: "../ress/ProX.webp",
+  },
+  {
+    name: "C922",
+    description: "The C922 is a streaming camera with 1080p video recording.",
+    Rating: "Rating:9.2/10",
+    Price: "Price:100$",
+    img: "../ress/C922.webp",
+  },
+  {
+    name: "G840",
+    description: "The G840 is a gaming mouse pad collaberation with Shroud.",
+    Rating: "Rating:9.2/10",
+    Price: "Price:100$",
+    img: "../ress/shroud-Pad.webp",
+  },
+  {
+    name: "G840 K/DA",
+    description:
+      "The G840 K/DA is a gaming mouse pad collaberation with League Of Legends.",
+    Rating: "Rating:9.2/10",
+    Price: "Price:100$",
+    img: "../ress/G840-K-DA.webp",
+  },
+];
+
+const cardContainer = document.querySelector(".products-cards");
+
+for (var i = 0; i < allProducts.length; i++) {
+  const cardDiv = document.createElement("div");
+  cardDiv.classList.add("card");
+  var img = document.createElement("img");
+  img.src = allProducts[i].img;
+  const cardBodyDiv = document.createElement("div");
+  cardBodyDiv.classList.add("card-body");
+  const cardTitle = document.createElement("h5");
+  cardTitle.classList.add("card-title");
+  cardTitle.innerHTML = allProducts[i].name;
+  const cardText = document.createElement("p");
+  cardText.classList.add("card-text");
+  cardText.innerHTML = allProducts[i].description;
+  const listGroup = document.createElement("ul");
+  listGroup.classList.add("list-group");
+  listGroup.classList.add("list-group-flush");
+  const Rating = document.createElement("li");
+  Rating.classList.add("list-group-item");
+  Rating.innerHTML = allProducts[i].Rating;
+  const Price = document.createElement("li");
+  Price.classList.add("list-group-item");
+  Price.innerHTML = allProducts[i].Price;
+
+  cardDiv.appendChild(img);
+  cardDiv.appendChild(cardBodyDiv);
+  cardBodyDiv.appendChild(cardTitle);
+  cardBodyDiv.appendChild(cardText);
+  cardDiv.appendChild(listGroup);
+  listGroup.appendChild(Rating);
+  listGroup.appendChild(Price);
+  cardContainer.appendChild(cardDiv);
+}
